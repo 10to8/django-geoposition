f8cce4ce7afeed6b09fbe8441a544769fe43e2ff
Richard Hills <richard.hills@10to8.com>
2025-01-24 12:25:13 +0000

    Fix for django4

--- geoposition/forms.py
+++ geoposition/forms.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 from .widgets import GeopositionWidget
 
